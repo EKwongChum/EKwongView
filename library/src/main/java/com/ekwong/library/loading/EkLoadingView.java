acd89c0321ddba2f98a3e69d55037e9ee4ca1237
EKwong <qinerjiang@banggood.com>
2018-11-12 11:55:02 +0800

    调整加载中的动画

--- library/src/main/java/com/ekwong/library/loading/EkLoadingView.java
+++ library/src/main/java/com/ekwong/library/loading/EkLoadingView.java
@@ -1,6 +1,8 @@
 package com.ekwong.library.loading;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.drawable.Animatable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.AppCompatImageView;
@@ -9,6 +11,8 @@ import android.view.View;
 import android.widget.FrameLayout;
 
 import com.ekwong.library.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
@@ -20,16 +24,40 @@ public class EkLoadingView extends FrameLayout {
 
     private Animatable mAnim;
     private AppCompatImageView mImageView;
+    private CircleImageView mCircleImageView;
+    private int mCircleRes;
 
     public EkLoadingView(Context context) {
         this(context, null);
     }
 
     public EkLoadingView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
+    }
+
+    public EkLoadingView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.EkLoadingView, defStyle, 0);
+        mCircleRes = a.getResourceId(R.styleable.EkLoadingView_loading_src, 0);
+        a.recycle();
+
         mImageView = new AppCompatImageView(context);
         mImageView.setImageResource(R.drawable.anim_vector_loading);
+        mCircleImageView = new CircleImageView(context);
+        mCircleImageView.setImageDrawable(context.getDrawable(mCircleRes));
+        int value = px2dp(context, 24);
+        System.out.println("value=" + value);
+        mCircleImageView.setBorderWidth(value);
+        mCircleImageView.setBorderColor(getResources().getColor(R.color.color_transparent));
         addView(mImageView);
+        addView(mCircleImageView);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 
     @Override
@@ -56,6 +84,11 @@ public class EkLoadingView extends FrameLayout {
                 }
             }
         }
+    }
+
+    private int px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
     }
 
 }
